f8fb54bccc75ddcaff12bcb32b71d8f862c99072
nerdchild6 <posorun.posorungg@gmail.com>
2024-11-07 07:47:01 +0700

    1

--- lib/student/home_screen.dart
+++ lib/student/home_screen.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:borrow_movie/login_student.dart';
 import 'package:borrow_movie/student/borrow_screen.dart';
+import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,6 +15,84 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final String url = '172.25.199.86:3000';
+  bool isWaiting = false;
+  String username = '';
+  List? asset;
+
+  void popDialog(String title, String message) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Text(message),
+          );
+        });
+  }
+
+  void getExpenses() async {
+    setState(() {
+      isWaiting = true;
+    });
+    try {
+      // get JWT token from local storage
+      final prefs = await SharedPreferences.getInstance();
+      String? token = prefs.getString('token');
+
+
+      if (token == null) {
+        // no token, jump to login page
+        // check mounted to use 'context' for navigation in 'async' method
+        if (!mounted) return;
+        // Cannot use only pushReplacement() because the dialog is showing
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => LoginScreen()),
+        );
+      }
+
+
+      // token found
+      // decode JWT to get uername and role
+      final jwt = JWT.decode(token!);
+      Map payload = jwt.payload;
+
+
+      // get expenses
+      Uri uri = Uri.http(url, '/all_asset');
+      http.Response response =
+          await http.get(uri, headers: {'authorization': token}).timeout(
+        const Duration(seconds: 10),
+      );
+      // check server's response
+      if (response.statusCode == 200) {
+        // show expenses in ListView
+        setState(() {
+          // update username
+          // username = payload['username'];
+          // convert response to List
+          asset = jsonDecode(response.body);
+    debugPrint('expense : $asset');
+
+        });
+      } else {
+        // wrong username or password
+        popDialog('Error', response.body);
+      }
+    } on TimeoutException catch (e) {
+      debugPrint(e.message);
+      popDialog('Error', 'Timeout error, try again!');
+    } catch (e) {
+      debugPrint(e.toString());
+      popDialog('Error', 'Unknown error, try again!');
+    } finally {
+      setState(() {
+        isWaiting = false;
+      });
+    }
+  }
+
   void confirmLogout() {
     showDialog(
       context: context,
@@ -97,6 +180,12 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    getExpenses();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -176,43 +265,55 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildCategoryItem(String genre, BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => BorrowScreen(),
-            settings: RouteSettings(
-              arguments: <String, dynamic>{
-                'categorie': '$genre',
-              },
-            ),
-          ),
-        );
-      },
-      child: Card(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/$genre.png',
-              width: 80,
-              height: 80,
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(height: 8),
-            Text(genre),
-          ],
+  // Find the first asset with a matching category.
+  var matchingAsset = asset?.firstWhere(
+    (item) => item['asset_name'] == genre,
+    orElse: () => null,
+  );
+
+  String filePath = matchingAsset != null
+      ? 'assets/images/categories/${matchingAsset['asset_name']}.png'
+      : 'assets/images/categories/$genre.png'; // Default if not found
+
+  return InkWell(
+    onTap: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => BorrowScreen(),
+          settings: RouteSettings(
+  arguments: <String, dynamic>{
+    'categorie': matchingAsset?['asset_name'] ?? genre,
+  },
+),
+
         ),
+      );
+    },
+    child: Card(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            filePath,
+            width: 80,
+            height: 80,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 8),
+          Text(genre),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
+
 
   Widget _buildRecommendedItem(String imageName) {
     return Padding(
       padding: const EdgeInsets.only(left: 16, right: 8),
       child: Image.asset(
-        'assets/images/$imageName',
+        'assets/images/categories/$imageName',
         width: 250,
         fit: BoxFit.cover,
       ),
